a41c89c627ef827836ba3727114f3ce71a3abac5
jaypii <jpomv2@icloud.com>
2021-01-04 23:20:37 +0100

    modified Symfony 5 App pagination

--- src/Controller/NWN2CharController.php
+++ src/Controller/NWN2CharController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Include paginator interface
+use Knp\Component\Pager\PaginatorInterface;
+
 #[Route('nwn2chars')]
 class NWN2CharController extends AbstractController
 {
